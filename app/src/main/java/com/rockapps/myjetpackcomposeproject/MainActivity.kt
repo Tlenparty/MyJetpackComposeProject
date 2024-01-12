f4f751d48502256e83ed80db27ccf8923b6aa88e
tlenparty <tevez-32@inbox.ru>
2024-01-12 12:29:12 +0300

    Add profile card

--- app/src/main/java/com/rockapps/myjetpackcomposeproject/MainActivity.kt
+++ app/src/main/java/com/rockapps/myjetpackcomposeproject/MainActivity.kt
@@ -3,13 +3,21 @@ package com.rockapps.myjetpackcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rockapps.myjetpackcomposeproject.ui.theme.MyJetpackComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +27,9 @@ class MainActivity : ComponentActivity() {
             MyJetpackComposeProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -30,14 +37,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.White,
+            elevation = 4.dp
+        ) {
+
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                //Column(modifier = Modifier.padding(5.dp)) {
+                    Text(
+                        text = "Miles. P",
+                        style = MaterialTheme.typography.h4,
+                        color = MaterialTheme.colors.primaryVariant
+                    )
+               // }
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.01f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_person),
+            contentDescription = "profile_image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyJetpackComposeProjectTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
